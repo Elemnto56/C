ae123c821ca854dbc61e088b8f686c7199035af3
Jaylon Christy <jaylon_christy@yahoo.com>
2025-05-16 21:20:33 -0500

    Reset all

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-    char string[] = "Hello World";
-    char *ptr = string;
-
-    printf("%p\n", ptr);
-    printf("%s\n", string);
+    printf("Hello World");
+    return 0;
 }
